126745aea64b6e412ad9f21c7d22b438ac3a0035
Tushar <you@example.com>
2024-04-29 14:49:12 +0530

    working on client

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,36 +1,50 @@
-"use client"
-import React, { useEffect, useState } from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import Styles from "../styles/chatBox.module.css";
 import { socket } from "@/app/socket";
 export default function ChatBox() {
+  const chatBoxRef = useRef<HTMLDivElement | null>(null);
   const [Messages, setMessages] = useState<string[]>([]);
-  const [content, setcontent] = useState("")
-    const sendMessage=()=>{
-        socket.emit("sendMessage",content)
-        setcontent("")
+  const [content, setcontent] = useState("");
+  const sendMessage = () => {
+    socket.emit("sendMessage", content);
+    setcontent("");
+  };
+  useEffect(() => {
+    socket.on("receiveMessage", (data: string) => {
+      setMessages((p) => [...p, data]);
+    });
+  }, []);
+  useEffect(() => {
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
     }
-    useEffect(()=>{
-        socket.on("receiveMessage",(data:string)=>{
-            console.log(data)
-            
-            setMessages(p=>[...p,data])
-        })
-    },[])
-    useEffect(()=>{
-        console.log(Messages)
-    },[Messages])
+  }, [Messages]);
   return (
     <div className={Styles.container}>
-      <div className={Styles.chats}>
-        {
-            Messages.length>0 && Messages.map(data=>{
-                return <div className={Styles.message} key={data}> {data}</div>
-            })
-        }
+      <div className={Styles.chats} ref={chatBoxRef}>
+        {Messages.length > 0 &&
+          Messages.map((data) => {
+            return (
+              <div className={Styles.message} key={data}>
+                {" "}
+                {data}
+              </div>
+            );
+          })}
       </div>
-    <div className={Styles.inputContainer}>   <input type="text" placeholder="type..." value={content} onChange={(e)=>setcontent(e.target.value)} onKeyPress={(e)=>{
-        e.key==="Enter" && sendMessage()
-    }} /> </div>
+      <div className={Styles.inputContainer} >
+        {" "}
+        <input
+          type="text"
+          placeholder="type..."
+          value={content}
+          onChange={(e) => setcontent(e.target.value)}
+          onKeyPress={(e) => {
+            e.key === "Enter" && sendMessage();
+          }}
+        />{" "}
+      </div>
     </div>
   );
 }
