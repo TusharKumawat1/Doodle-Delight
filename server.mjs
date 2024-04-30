16c216310c4fbb866937827784069c00b0722875
Tushar <you@example.com>
2024-04-30 15:12:40 +0530

    quick fix

--- server.mjs
+++ server.mjs
@@ -9,38 +9,53 @@ const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
   const httpServer = createServer(handler);
-  const io = new Server(httpServer);
-  let AllUser=[]// todo
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+    },
+  });
+  let AllUser = []; // todo
   io.on("connection", (socket) => {
     console.log("new user joined");
-    socket.on("joinRoom",PayLoad=>{
-      socket.join(PayLoad.roomId)
-      const UserExists=AllUser.findIndex(user=>user.userId===PayLoad.userId)
-      if (UserExists===-1) {
-        AllUser.push(PayLoad)
-      }else{
-        AllUser[UserExists]=PayLoad
+    socket.on("joinRoom", (PayLoad) => {
+      socket.join(PayLoad.roomId);
+      const UserExists = AllUser.findIndex(
+        (user) => user.userId === PayLoad.userId
+      );
+      if (UserExists === -1) {
+        AllUser.push(PayLoad);
+      } else {
+        AllUser[UserExists] = PayLoad;
       }
-      io.to(PayLoad.roomId).emit("users",AllUser.filter(data=>data.roomId===PayLoad.roomId))
-      socket.emit("users", AllUser.filter((data) => data.roomId === PayLoad.roomId));
-      socket.on("removeUser",(userId)=>{
-        const UserExists=AllUser.findIndex(user=>user.userId===userId)
-        if (UserExists===-1) {
-          console.log("No user found")
-        }else{
-          AllUser.splice(UserExists,1)
+      io.to(PayLoad.roomId).emit(
+        "users",
+        AllUser.filter((data) => data.roomId === PayLoad.roomId)
+      );
+      socket.emit(
+        "users",
+        AllUser.filter((data) => data.roomId === PayLoad.roomId)
+      );
+      socket.on("removeUser", (userId) => {
+        const UserExists = AllUser.findIndex((user) => user.userId === userId);
+        if (UserExists === -1) {
+          console.log("No user found");
+        } else {
+          AllUser.splice(UserExists, 1);
         }
-        io.in(PayLoad.roomId).emit("userExit",AllUser.filter(data=>data.roomId===PayLoad.roomId))
-      })
-      socket.on("sendMessage",data=>{
-        io.in(PayLoad.roomId).emit("receiveMessage",data)
-      })
-      socket.on("draw",data=>{
-        io.to(PayLoad.roomId).emit("loadData",data)
-      })
-    })
+        io.in(PayLoad.roomId).emit(
+          "userExit",
+          AllUser.filter((data) => data.roomId === PayLoad.roomId)
+        );
+      });
+      socket.on("sendMessage", (data) => {
+        io.in(PayLoad.roomId).emit("receiveMessage", data);
+      });
+      socket.on("draw", (data) => {
+        io.to(PayLoad.roomId).emit("loadData", data);
+      });
+    });
   });
-  
+
   httpServer
     .once("error", (err) => {
       console.log(err);
