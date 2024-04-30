8b0d907ad12da39dfc49659f084a7c1b6b9b7c03
Tushar <you@example.com>
2024-04-30 15:39:42 +0530

    fixing socket server

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,2 +1,4 @@
 import io from "socket.io-client"
-export const socket =io("https://doodle-delight.vercel.app");
+export const socket = io("https://doodle-delight.vercel.app", {
+    transports: ["websocket"],
+});
