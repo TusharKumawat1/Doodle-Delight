c07723ca92238a21a067e72b4061e58a37f3792a
Tushar <you@example.com>
2024-05-01 15:13:37 +0530

    change in socket

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,8 +1,3 @@
 import io from "socket.io-client"
 const URl= process.env.NODE_ENV===`production`?`https://doodle-delight.vercel.app`: `http://localhost:3000`
-export const socket = io(URl, {
-    path:"/socket",
-    transports: ["websocket","polling"],
-    reconnection:true,
-    reconnectionAttempts:5
-});
+export const socket = io(URl, );
