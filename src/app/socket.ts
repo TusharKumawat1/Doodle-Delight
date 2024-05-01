351190a5bbec88e9fe1fc220a0ef4108555e370b
Tushar <you@example.com>
2024-05-01 15:15:35 +0530

    some chnagese

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,3 +1,8 @@
 import io from "socket.io-client"
 const URl= process.env.NODE_ENV===`production`?`https://doodle-delight.vercel.app`: `http://localhost:3000`
-export const socket = io(URl, );
+export const socket = io(URl, {
+    path:"/socket",
+    transports: ["websocket"],
+    reconnection:true,
+    reconnectionAttempts:5
+});
